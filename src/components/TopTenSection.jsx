97538994f6654925730d6139a503f60d42746638
KatheRojasO <kathe.rojaso@gmail.com>
2023-04-04 14:18:14 +0200

    refactor browse page

---
+++ src/components/TopTenSection.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import data from "../JSONFiles/topTenNumbers.json";
+
+export default function TopTenSection() {
+  const firstSection = data.slice(0, 5);
+  const secondSection = data.slice(5, 10);
+
+  const section1 = firstSection.map((section) => (
+    <div key={section.id} className="item">
+      <img src={section.image} alt="number-img" className="number-img" />
+      <img src={section.movieImage} alt="movie-img" className="movie-img" />
+    </div>
+  ));
+
+  const section2 = secondSection.map((section) => (
+    <div key={section.id} className="item">
+      <img src={section.image} alt="number-img" className="number-img" />
+      <img src={section.movieImage} alt="movie-img" className="movie-img" />
+    </div>
+  ));
+  return (
+    <div>
+      <div className="container">
+        <p>Top 10 TV Shows & Movies in Sweden Today</p>
+        <div className="wrapper">
+          <section id="firstSection">
+            <a href="#secondSection">‹</a>
+            {section1}
+            <a href="#secondSection">›</a>
+          </section>
+          <section id="secondSection">
+            <a href="#firstSection">‹</a>
+            {section2}
+            <a href="#firstSection">›</a>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
