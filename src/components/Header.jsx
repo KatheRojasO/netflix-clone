3779995360b778683d3f9e8f3903f965286f8e95
KatheRojasO <kathe.rojaso@gmail.com>
2023-04-11 00:03:34 +0200

    Fix css header for every device

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/netflix-logo.png";
 import userLogo from "../assets/images/user-logo.jpg";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import data from "../JSONFiles/navbar.json";
 import SearchBar from "./SearchBar";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+    document.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled]);
+
   const navItems = data.map((item) => (
-    <li className="nav-item" key={item.id}>
+    <div className="link-container" key={item.id}>
       <Link to={item.href}>{item.text}</Link>
-    </li>
+    </div>
   ));
 
   return (
-    <header className="netflix-header">
+    <header className={`netflix-header ${scrolled ? 'scrolled' : ''}` }>
       <div className="header-container">
         <div className="nav-left">
           <Link to="/browse">
             <img src={logo} alt="netflix-logo" className="logo"></img>
           </Link>
-          <nav>
-            <ul className="menu">{navItems}</ul>
+          <nav className="dropdown">
+            <button class="dropbtn">
+              Browse <ArrowDropDownIcon />
+            </button>
+            <div className="dropdown-content">{navItems}</div>
           </nav>
         </div>
+
         <div className="nav-right">
           <SearchBar />
-          <img src={userLogo} alt="netflix-logo" className="user-logo"></img>
+          <img src={userLogo} alt="user-logo" className="user-logo"></img>
         </div>
       </div>
     </header>
