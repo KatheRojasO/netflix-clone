a75fb9445d499cd80d3d6ddd0c316c5a63ff72a1
KatheRojasO <kathe.rojaso@gmail.com>
2023-04-14 20:48:09 +0200

    Implement episodes to user modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ReactPlayer from "react-player/youtube";
 import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+import { Link } from "react-router-dom";
 
 export default function Modal({ videoData, open, onClose }) {
   const { link, description, genres, duration, year, cast, seasons } =
@@ -9,21 +10,33 @@ export default function Modal({ videoData, open, onClose }) {
 
   if (!open) return null;
 
-  const episodes = seasons ? seasons.map((item) => {
-    return (
-      <div key={item.id}>
-        <h1>{item.season}</h1>
-        {item.episodes.map((episode) => {
-          return (
-            <div>
-              {episode.title}
-              {episode.description}
+  const episodes = seasons
+    ? seasons.map((item) => {
+        return (
+          <div key={item.id}>
+            <div className="season-card-header">
+              <h3>Episodes</h3>
+              <h3>{item.season}</h3>
             </div>
-          )
-        })}
-      </div>
-    );
-  }) : null;
+            {item.episodes.map((episode) => {
+              return (
+                <div className="episodes-container">
+                  <div className="episode-card-header">
+                    <h4>{episode.title}</h4>
+                    <p>{episode.duration}</p>
+                  </div>
+                  <div className="episode-description">
+                    <Link to={episode.link}>
+                      <p>{episode.description}</p>
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })
+    : null;
 
   return ReactDOM.createPortal(
     <div className="modal">
@@ -37,7 +50,7 @@ export default function Modal({ videoData, open, onClose }) {
           <div className="modal-info-left">
             <div className="header">
               <span>{year}</span>
-              { duration === undefined && <span>{duration}</span>}
+              {duration === undefined && <span>{duration}</span>}
             </div>
             <div className="modal-description">
               <p>{description}</p>
@@ -53,13 +66,8 @@ export default function Modal({ videoData, open, onClose }) {
               {genres}{" "}
             </p>
           </div>
-          
-
         </div>
-        { episodes && 
-          <div>episodes {episodes}</div>
-          
-        }
+        {episodes && <div className="modal-info-down">{episodes}</div>}
       </div>
     </div>,
     document.getElementById("portal")
